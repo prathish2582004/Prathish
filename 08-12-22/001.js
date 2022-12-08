3236d5c14dae4e3f13b6ffdea69864d858acf4f7
prathish2582004 <118422623+prathish2582004@users.noreply.github.com>
2022-12-08 15:43:44 +0530

    08-12-22
    JAVA SCRIPT

---
+++ 08-12-22/001.js
@@ -0,0 +1,9 @@
+let name="vimal";
+ console.log(name)
+function namenew(){
+    const name ="mohan";
+    return console.log(name);
+}
+
+namenew()
+// consolo.log(name)
---
+++ 08-12-22/002.js
@@ -0,0 +1,16 @@
+var name0 ="skct"
+
+let name1="mohan"
+
+const name2="king"
+
+function newname(){
+    var name0 = "skct"
+    let name1 = "vimal"
+    const name3 = "king"
+
+    const.log(name0,name1,name2)
+}
+
+newname()
+consolo.log(name0,name1,name2)
